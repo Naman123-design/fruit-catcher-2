97eb8e653f25ca8aa45c7a2aba668176c111d531
Naman123-design <70699248+Naman123-design@users.noreply.github.com>
2020-11-03 19:33:48 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,50 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+    getPlayerScore() {
+        console.log("get player score is working")
+        database.ref('fruitsCatched').on("value",(data)=>{
+          this.score = data.val();
+        })
+      }
+  
+    
+      static updatePlayersScore(score) {
+        database.ref('/').update({
+            fruitsCatched:score
+        })
+      }
+}
